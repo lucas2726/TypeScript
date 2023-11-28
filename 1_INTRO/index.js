a5847ce2d37e8289eaeb6a7992454c33e1148c28
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-11-28 11:14:52 -0400

    ss

---
+++ 1_INTRO/index.js
@@ -0,0 +1,7 @@
+var firtName = 'Matheus';
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá" + name);
+}
+greeting(firtName);
---
+++ 2_fundamentos/index.ts
@@ -0,0 +1,7 @@
+//inteference e annotation
+let ann: string = 'Teste'
+
+let inf = "Teste"
+
+inf = 1
+ann = 1
